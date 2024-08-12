dc6341d8a3a8210fa37faaacaeaa773e64f4d533
sdvenus <dahiana-s@hotmail.com>
2024-08-12 02:28:01 -0300

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Replace 'your-repo-name' with the actual name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // This is required for GitHub Pages
+});
